36681405a9b90f4fd6380baa9dfe885271d0396a
Sabhayor <ayodejisaberedowo@gmail.com>
2025-09-24 12:12:40 +0100

    all required files added

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from ECS + GitHub Actions!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
